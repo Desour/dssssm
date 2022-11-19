b81c32e084d2a3c1ec90a99955a491b693ea2043
cx384 <muelladresse84@web.de>
2022-11-19 13:58:23 +0100

    add more rocks

--- README.txt
+++ README.txt
@@ -5,10 +5,13 @@
 
 ### Code
 
-MIT (Expat) by DS
+MIT (Expat) by DS and cx384
 
 ### Media
 
 * `mods/dssssa_player/textures/dssssa_skybox_*.jpg`, `menu/header.png`, `menu/icon.png`:
   by Westbeam (WTFPL) (or based on their art, with trivial changes)
   https://opengameart.org/content/space-skybox-0
+
+* `mods/dssssa_rocks/textures/dssssa_rocks_rock*`:
+  by cx384 (CC-BY-SA-4.0)
--- mods/dssssa_rocks/init.lua
+++ mods/dssssa_rocks/init.lua
@@ -1,7 +1,7 @@
 
 minetest.register_node("dssssa_rocks:rock1", {
-	description = "Rock",
-	groups = {cracky = 3},
+	description = "Light Gray Rock",
+	groups = {cracky = 2},
 	drawtype = "normal",
 	tiles = {"dssssa_rocks_rock1.png"},
 	paramtype = "none",
@@ -12,4 +12,74 @@ minetest.register_node("dssssa_rocks:rock1", {
 		--~ dug = <SimpleSoundSpec>,
 		--~ place = <SimpleSoundSpec>,
 	--~ },
+})
+
+minetest.register_node("dssssa_rocks:rock2", {
+	description = "Gray Rock",
+	groups = {cracky = 3},
+	drawtype = "normal",
+	tiles = {"dssssa_rocks_rock2.png"},
+	paramtype = "none",
+	paramtype2 = "none",
+	is_ground_content = false,
+})
+
+minetest.register_node("dssssa_rocks:rock3", {
+	description = "Dark Gray Rock",
+	groups = {cracky = 4},
+	drawtype = "normal",
+	tiles = {"dssssa_rocks_rock3.png"},
+	paramtype = "none",
+	paramtype2 = "none",
+	is_ground_content = false,
+})
+
+minetest.register_node("dssssa_rocks:rock4", {
+	description = "Yellow Rock",
+	groups = {cracky = 2},
+	drawtype = "normal",
+	tiles = {"dssssa_rocks_rock4.png"},
+	paramtype = "none",
+	paramtype2 = "none",
+	is_ground_content = false,
+})
+
+minetest.register_node("dssssa_rocks:rock5", {
+	description = "Light Blue Rock",
+	groups = {cracky = 4},
+	drawtype = "normal",
+	tiles = {"dssssa_rocks_rock5.png"},
+	paramtype = "none",
+	paramtype2 = "none",
+	is_ground_content = false,
+})
+
+minetest.register_node("dssssa_rocks:rock6", {
+	description = "Red Rock",
+	groups = {cracky = 3},
+	drawtype = "normal",
+	tiles = {"dssssa_rocks_rock6.png"},
+	paramtype = "none",
+	paramtype2 = "none",
+	is_ground_content = false,
+})
+
+minetest.register_node("dssssa_rocks:rock7", {
+	description = "Dark Blue Rock",
+	groups = {cracky = 3},
+	drawtype = "normal",
+	tiles = {"dssssa_rocks_rock7.png"},
+	paramtype = "none",
+	paramtype2 = "none",
+	is_ground_content = false,
+})
+
+minetest.register_node("dssssa_rocks:rock8", {
+	description = "Cyan Rock",
+	groups = {cracky = 4},
+	drawtype = "normal",
+	tiles = {"dssssa_rocks_rock8.png"},
+	paramtype = "none",
+	paramtype2 = "none",
+	is_ground_content = false,
 })
